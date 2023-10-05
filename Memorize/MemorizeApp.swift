c152904d9d18b0560fc45414e06ee51c426836f6
mlwelker <nutria.watts.0g@icloud.com>
2023-10-05 12:56:03 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Michael Welker on 2023-10-05.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
