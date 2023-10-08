2edc4e979e2fe2133c17343188814aa48818a242
mlwelker <nutria.watts.0g@icloud.com>
2023-10-08 17:13:38 -0500

    refactored RoundedRectangle to base isFaceUp toggle

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -5,7 +5,7 @@ struct ContentView: View {
     var body: some View {
         HStack {
             CardView()
-            CardView(isFaceUp: true)
+            CardView()
             CardView()
             CardView()
         }
@@ -15,19 +15,21 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State private var isFaceUp = false
     
     var body: some View {
         ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundStyle(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .stroke(lineWidth: 3)
+                base.fill(.white)
+                base.stroke(lineWidth: 3)
                 Text("👻").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                base.fill()
             }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
