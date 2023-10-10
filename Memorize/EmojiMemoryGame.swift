7ac9f4218e3aaa44e85d418f336f5ccff9c0fb73
mlwelker <nutria.watts.0g@icloud.com>
2023-10-10 12:19:20 -0500

    MVVM

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,33 @@
+
+import SwiftUI
+
+class EmojiMemoryGame: ObservableObject {
+    private static let emojis = ["👻", "😈", "🕷️", "🎃", "💀", "🕸️", "🧙‍♀️", "🙀", "👹", "😱", "☠️", "🍭"]
+    
+    private static func createMemoryGame() -> MemoryGame<String> {
+        return MemoryGame(numberOfPairsOfCards: 12) { pairIndex in
+            if emojis.indices.contains(pairIndex) {
+                return EmojiMemoryGame.emojis[pairIndex]
+            } else {
+                return "⁉️"
+            }
+        }
+    }
+    
+    @Published private var model = EmojiMemoryGame.createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+    // MARK: - Intents
+    
+    func shuffle() {
+        model.shuffle()
+    }
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
+    }
+}
+
---
+++ Memorize/EmojiMemoryGameView.swift
@@ -0,0 +1,59 @@
+
+import SwiftUI
+
+struct EmojiMemoryGameView: View {
+    @ObservedObject var viewModel: EmojiMemoryGame
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                cards
+            }
+            Button("Shuffle") {
+                viewModel.shuffle()
+            }
+        }
+        .padding()
+    }
+    
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
+            ForEach(viewModel.cards.indices, id: \.self) { i in
+                CardView(viewModel.cards[i])
+                    .aspectRatio(2/3, contentMode: .fit)
+                    .padding(4)
+            }
+        }
+        .foregroundStyle(.orange)
+    }
+}
+
+struct CardView: View {
+    let card: MemoryGame<String>.Card
+    
+    init(_ card: MemoryGame<String>.Card) {
+        self.card = card
+    }
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.fill(.white)
+                base.stroke(lineWidth: 3)
+                Text(card.content)
+                    .font(.system(size: 120))
+                    .minimumScaleFactor(0.01)
+                    .aspectRatio(1, contentMode: .fit)
+            }
+            .opacity(card.isFaceUp ? 1 : 0)
+            
+            base.fill().opacity(card.isFaceUp ? 0 : 1)
+        }
+    }
+}
+
+#Preview {
+    EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+}
